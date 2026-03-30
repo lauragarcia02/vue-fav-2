b26342dc2c3d832de463cceb2f80993ab090c449
Laura García <lauragarciaramirezbj@gmail.com>
2026-03-30 16:46:12 +0200

    lgarram: Comenzamos la página de Batman

---
+++ src/paginae/batman/Batman.vue
@@ -0,0 +1,93 @@
+<script lang="ts" setup>
+import { House } from 'lucide-vue-next'
+</script>
+
+
+
+<template>
+  
+<header>
+
+    <div class="batman">
+
+    <RouterLink to="/">
+        <House class="icon-home" />
+    </RouterLink>
+
+    <header class="titulus">
+        <h1>Batman</h1>
+
+        <div id="titulus-batman" class="titulus-img">
+            
+        </div>
+
+        <p> Él puede tomar la decisión que nadie más puede, la decisión correcta.</p>
+
+    </header>
+
+    </div>
+
+</header>
+
+</template>
+
+<style scoped>
+
+.icon-home{
+    color: slateblue;
+    width: 3rem;
+    height: 3rem;
+}
+
+.icon-home:hover{
+    color: white;
+    width: 3rem;
+    height: 3rem;
+    background-color: slateblue;
+}
+
+.batman{
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.titulus-img {
+  background-size: 100% 100%;
+  background-position: center center;
+  background-image: url("../imagines/batman/batman.jpg");
+  min-height: 100vh;
+}
+
+.titulus-img:hover {
+  background-size: 115% 115%;
+}
+
+.titulus > h1 {
+  position: absolute;
+  top: 63%;
+  width: 100%;
+  text-align: center;
+  font-size: 5rem; 
+  color: white;
+  pointer-events:none; 
+  font-family: batman;
+}
+
+.titulus > p {
+  position: absolute;
+  top: 36%;
+  width: 100%;
+  text-align: center;
+  font-size: 2rem;
+  color: white;
+  pointer-events:none;
+  display: none;
+  font-family: batman;
+}
+
+@media (min-width: 768px) {
+    titulus > p{
+        display: block;
+    }
+}
+
+</style>
---
+++ src/paginae/domus/Domus.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+
+<template>
+
+<div class="fondo-home">
+  
+    <h1>
+      <span>Diseño</span> Web y 
+      <br>
+      Apps <span>Interactivas</span>
+    </h1>
+  
+  <div class="botones">
+
+        <Button>
+            <RouterLink to="/indecision"> Si o No </RouterLink>
+        </Button>
+
+        <Button>
+            <RouterLink to="/batman"> Batman </RouterLink>
+        </Button>
+
+        <Button>
+            <RouterLink to="/simpson"> Simpson </RouterLink>
+        </Button>
+
+        <Button>
+            <a href="https://www.upv.es"> UPV </a>
+        </Button>
+
+    </div>
+
+</div>
+  
+</template>
+
+<style scoped>
+
+.fondo-home {
+  background-color: rgb(21, 25, 52);
+  height: 100vh;
+  color: rgb(24, 182, 246);
+}
+
+
+.botones > Button {
+  margin-right: 0.5rem;
+  background-color: rgb(24, 182, 246);
+} 
+
+
+.botones > Button:hover {
+  background-color: rgba(24, 182, 246, 0.7);
+} 
+
+</style>
