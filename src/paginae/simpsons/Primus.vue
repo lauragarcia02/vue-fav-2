c7f79b08dbc7652deae4e59f5c00facf94232850
Nombre Apellido <alumno@gmail.com>
2026-04-21 17:22:58 +0200

    lgarram: Página de inicio de Simpsons

--- src/paginae/simpsons/Primus.vue
+++ src/paginae/simpsons/Primus.vue
@@ -1,8 +1,44 @@
+<script lang="ts" setup>
+import LogoSimpsons from '@/components/LogoSimpsons.vue';
+import { Button } from '@/components/ui/button';
+
+
+</script>
+
 
 <template>
   
-    <h1 class="text-6xl m-8 text-red-500 bg-amber-300 w-fit px-12 py-5 rounded-2xl shadow-xl hover:bg-red-500 hover:text-white ">
-    Simpsons Page
-    </h1>
+<div class="bg-[#ffde00] min-h-screen text-center p-4 flex flex-col items-center justify-center">
+
+    <LogoSimpsons estilus="w-[24rem]" />
+
+    <h1 class="text-xl my-1 font-medium">
+    	Discover some of the most amazing Quotes from The Simpsons
+	</h1>
+
+	<div>
+    	<strong>This is an unofficial app (even though it looks so cool!)</strong>
+	</div>
+
+	<div className="mt-8 flex gap-5">
+		<Button variant="tertiarius" size="elongatis" >
+      		<RouterLink to="/">Exit</RouterLink>
+    	</Button>
+    	<Button variant="tertiarius" size="elongatis"> 
+      		<RouterLink to="">Enter</RouterLink>
+    	</Button>
+	</div>
+
+    <div class="flex gap-12">
+
+        <img class="object-contain hidden md:block" src="/imagines/simpsons/bart.png" alt="" width="200px">
+        <img class="object-contain hidden md:block mt-20" src="/imagines/simpsons/homer.png" alt="" width="200px">
+        <img class="object-contain mt-9 md:mt-0" src="/imagines/simpsons/moe.png" alt="" width="220px">
+
+    </div>
+
+
+
+</div>
 
 </template>
