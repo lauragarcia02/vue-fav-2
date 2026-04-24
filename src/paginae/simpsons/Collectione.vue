1e261c9824ec9aac5f744cbf637c821e49343ca3
Laura García <lauragarciaramirezbj@gmail.com>
2026-04-24 17:51:30 +0200

    Header y footer del Layout

--- src/paginae/simpsons/Collectione.vue
+++ src/paginae/simpsons/Collectione.vue
@@ -1,8 +1,12 @@
+import path from 'node:path';
 <script lang="ts" setup></script>
 
 <template>
-    <div>
-        <h1>Página de galería</h1>
+    <div class="flex flex-col items-center justify-center gap-8 w-full max-w-[1400px] my-8">
+        <h1 class="font-bold text-xl lg:text-5xl mb-5">
+            The Simpson Quote App
+        </h1>
+        <p class="mb-4">Click on the quote of your favorite character</p>
     </div>
 </template>
 
