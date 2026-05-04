4e141f9d8a2e6871503f7be717045373671bc674
Laura García <lauragarciaramirezbj@gmail.com>
2026-05-04 12:16:10 +0200

    lgarram: Buscador de personajes de Shadcn

--- src/paginae/simpsons/Collectione.vue
+++ src/paginae/simpsons/Collectione.vue
@@ -5,8 +5,9 @@ import {
 import CardContent from '@/components/ui/card/CardContent.vue';
 import { characters } from './data';
 import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
- //const listaSimpsons = ref( characters.slice(0, 6) )
+const router = useRouter()
 
 const inPagina = 6
 
@@ -31,7 +32,7 @@ const paginaMumeri: number[] = [ ... Array(totalPaginae)].map( (_, i) => i + 1 )
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center gap-8 w-full max-w-[1400] my-8">
+    <div class="flex flex-col items-center justify-center gap-8 w-full max-w-[1400] my-8 mx-auto">
         
         <div class="text-center ">
             <h1 class="font-bold text-xl lg:text-5xl mb-5">
@@ -46,12 +47,15 @@ const paginaMumeri: number[] = [ ... Array(totalPaginae)].map( (_, i) => i + 1 )
             :key="character.id"
             class="cursor-pointer w-[220px] h-[240px] hover:bg-[#ee3133] hover:text-white transition-all"
             >
-                <CardContent class="flex flex-col items-center gap-1 w-full px-0">
+                <CardContent 
+                    class="flex flex-col items-center gap-1 w-full px-0"
+                    @click="router.push(`/simpsons/gallery/${ character.id }`)"
+                    >
                     <img 
                         :src="`/imagines/simpsons/${ character.imago }`" 
                         alt=""
                         class="w-48 h-42 object-cover object-top mt-2 bg-[#ffde00] border border-black rounded-t-md">
-                    <h2>Homer Simpson</h2>
+                    <h2 class="font-medium text-lg"> {{ character.nomen }}</h2>
                 </CardContent>
             </Card>
         </div>
